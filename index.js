c4c799ebf31605bf6d12cf134ef6586ee3758af4
Adam Abass <adam.imoro@student.moringaschool.com>
2022-05-13 08:54:01 +0000

    commit tree 49bf7035c516b93aab2a9cd187ef64a37a197228

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(driverArr){
+    return driverArr.slice(0,2);
+   }
+   
+   const returnLastTwoDrivers = function (driverArr){
+     return driverArr.slice(driverArr.length-2, driverArr.length)
+   }
+   
+   const selectingDrivers = [
+     returnFirstTwoDrivers, returnLastTwoDrivers
+   ];
+   
+   function createFareMultiplier(num){
+     return function(fare){
+       return fare * num;
+     }
+   }
+   
+   
